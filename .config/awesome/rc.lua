ca672e8ca01b18afd954215b4ee903f1ae3e2f78
Grégoire Juge <gregoire.juge@gmail.com>
2018-01-31 21:38:14 +0100

    [UPD] Fix indenting in rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -487,7 +487,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey }, "x", function ()
         awful.spawn(string.format("dmenu_run -i -fn 'Monospace' -nb '%s' -nf '%s' -sb '%s' -sf '%s'",
         beautiful.bg_normal, beautiful.fg_normal, beautiful.bg_focus, beautiful.fg_focus))
-		end,
+            end,
         {description = "show dmenu", group = "launcher"})
     --]]
     -- Prompt
